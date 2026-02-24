211cdef07577d6be007b2734df688ade54b8bb5a
Yevhenii Melnichuk <kn241_myev@student.ztu.edu.ua>
2026-02-24 17:32:58 +0200

    feat: task3 - implement constructor

--- lr3/v13/Users.php
+++ lr3/v13/Users.php
@@ -9,15 +9,29 @@ class Users
     public string $nickname;
     public string $name;
     public string $surname;
-    public string $age;
+    public int $age;
     public string $email;
     public string $password;
 
-    /**
-     * task2
-     */
+
     public function getInfo(): string
     {
         return "Ім'я: {$this->name}, Email: {$this->email}, Логін: {$this->nickname}, Пароль: {$this->password}";
+    }
+
+    public function __construct(
+        string $name = '',
+        string $email = '',
+        string $nickname = '',
+        string $password = '',
+        string $surname = '',
+        int $age = 0
+    ) {
+        $this->name = $name;
+        $this->email = $email;
+        $this->nickname = $nickname;
+        $this->password = $password;
+        $this->surname = $surname;
+        $this->age = $age;
     }
 }
