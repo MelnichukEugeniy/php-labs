433cd232f3b14d90beb015644baf1218e9b0f6ec
Мельничук Євгеній Володимирович <kn241_myev@student.ztu.edu.ua>
2026-04-15 16:42:41 +0300

    feat: copy v13 from lab4

---
+++ lr5/variants/v13/views/layout/footer.php
@@ -0,0 +1,9 @@
+</div>
+</main>
+<footer class="footer">
+    <div class="container">
+        <p>&copy; <?= date('Y') ?> Магазин мотоциклів &mdash; Варіант 13, Лабораторна робота №4</p>
+    </div>
+</footer>
+</body>
+</html>
